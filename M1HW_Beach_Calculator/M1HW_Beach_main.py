d84b5334a29bde8e9108bc8e0543138ff764c48a
Brenda Beach <90799109+beachb7704@users.noreply.github.com>
2023-08-28 21:29:35 -0400

    Add files via upload

---
+++ M1HW_Beach_Calculator/M1HW_Beach_main.py
@@ -0,0 +1,32 @@
+#// CTS 285
+#// M1HW
+#// Brenda Beach
+#// August 21, 2023
+
+
+
+
+# Import all of the other modules needed to run inside this module
+#=============#
+# Imports     #
+#=============#
+from M1HW_Beach_user_choice import *
+
+
+
+#=============#
+def main():
+#=============#
+        """ 
+    This function starts the program and looks into new module to show main menu to the user. 
+    
+    inputs: none
+    outputs: sent request to user_choice module and displays main menu
+        """
+        
+        user_input.start_menu()
+        
+
+
+if __name__ == '__main__':
+        main()
