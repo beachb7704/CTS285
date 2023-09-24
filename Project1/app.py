3c9d3b96a39d2016f4da089e0d208918a1e05a8d
Brenda Beach <90799109+beachb7704@users.noreply.github.com>
2023-09-24 19:49:00 -0400

    Add files via upload
    Updated code to add users to database

--- Project1/app.py
+++ Project1/app.py
@@ -3,27 +3,57 @@ from flask import Flask, redirect, render_template, request, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+# Below is what is typed in when trying to create the database in the terminal window
+#from app import app
+#from app import db
 
 
 
 # Create the Flask instance
 app = Flask(__name__)
+
+# Add a database table. This will reference the table of the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dataman.db'
+
 # Create a secret key to validate the form has not been tampered with
 app.config['SECRET_KEY'] = "secretkey"
 
+# Initialize the database
+db = SQLAlchemy(app)
+app.app_context().push()
 
-##############
-#FORM CLASSES#
-##############
+
+    
+    
+
+#########
+#CLASSES#
+#########
+
+# Create Model for adding users to database
+class students(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # Create a string
+    #def __repr__(self):
+    #    return'<username %r>' % self.username
 
 # Create a Form Class
 class create_account(FlaskForm):
+    username = StringField("Enter your username", validators=[DataRequired()])
     first_name = StringField("Enter your first name", validators=[DataRequired()])
     last_name = StringField("Enter your last name", validators=[DataRequired()])
-    username = StringField("Enter your username", validators=[DataRequired()])
     password = StringField("Enter your password", validators=[DataRequired()])
     submit = SubmitField("Submit")
-    cancel = SubmitField("Cancel")
+
+
 
 
 
@@ -48,27 +78,36 @@ def user_home(name):
 # Going to pull up the form from the class we created earlier.
 @app.route("/add_acct", methods=['GET','POST'])
 def create_acct():
+    username = None
     first_name = None
     last_name = None
-    username = None
     password = None
     form = create_account()
     
     # Validate Form
     if form.validate_on_submit():
+        # This will query the database to see if this username already exists
+        user = students.query.filter_by(username=form.username.data).first()
+        if user is None:
+            user= students(username=form.username.data, first_name=form.first_name.data, last_name=form.last_name.data, password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            
+        username = form.username.data
         first_name = form.first_name.data
         last_name = form.last_name.data
-        username = form.username.data
-        password = form.password.data
+        password = form.password.data    
         # This will clear the form out for the next submission
+        form.username.data = ''
         form.first_name.data = ''
         form.last_name.data = ''
-        form.username.data = ''
         form.password.data = ''
         # This will put a flash banner across the screen if the form was submitted successfully.
         flash("Account Created Successfully")
-    
-    return render_template("add_acct.html", first_name = first_name, last_name = last_name, username = username, password = password, form = form)
+        
+    # This will show the users in the database by the date added    
+    #our_users = students.query.order_by(students.date_added)    
+    return render_template("add_acct.html", form = form, username = username, first_name = first_name, last_name = last_name, password = password)
 
 
 
