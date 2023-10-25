864716fef6a9b1bb097f8c20f76eff760feb3531
MrChiff <105020263+MrChiff@users.noreply.github.com>
2023-10-25 13:45:47 -0400

    class 23/10/25: working on flash_cards, pass json into flash_card_set and parse with json.loads()

--- Project1/proto_hardwick/Mathmaticus/Question_Class.py
+++ Project1/proto_hardwick/Mathmaticus/Question_Class.py
@@ -2,19 +2,15 @@ from tabulate import tabulate
 
 class Question():
 
-    def __init__(self, quest_num, num1, operator, num2, ans, T_F):
+    def __init__(self, num1, operator, num2, ans, T_F):
 
-        self.quest_num = int(quest_num)
         self.num1 = int(num1)
         self.operator = operator
         self.num2 = int(num2)
         self.ans = int(ans)
         self.quest_true = bool(T_F)
 
-    # Getters
-    def get_quest_num(self):
-        return self.quest_num
-    
+    # Getters    
     def get_num1(self):
         return self.num1
     
@@ -31,10 +27,7 @@ class Question():
         return self.quest_true
     
     
-    # Setters
-    def set_quest_num(self, quest_num):
-        self.quest_num = quest_num
-        
+    # Setters        
     def set_num1(self, num1):
         self.num1 = num1
         
@@ -53,8 +46,7 @@ class Question():
     # String Representation:    
     def __repr__(self):
 
-        return tabulate([["Question Number:", str(self.quest_num)],
-                          ["1st Number:", str(self.num1)],
+        return tabulate([["1st Number:", str(self.num1)],
                           ["Operator:", self.operator],
                           ["2nd Number:", str(self.num2)],
                           ["Answer:", str(self.ans)]])
